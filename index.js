af969158894cf74b21ae7655f26cf505b27f6ac5
LoveAndHatesPython <98858926+LoveAndHatesPython@users.noreply.github.com>
2022-02-01 19:02:42 -0600

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-//Makes Thing Work As Well As Errors
-require("express")().use(require("express").static(__dirname + "/public")).use((_, res) => {
-  res.status(404).sendFile(__dirname + "/404.html");
-}).listen(8080);
-console.clear()
